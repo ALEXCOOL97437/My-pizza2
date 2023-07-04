7fe68d2544351febbe43b514cca421d1ec6abb90
WOLFF Alexandre <alex.wolff97437@gmail.com>
2023-07-04 20:34:44 +0400

    ajout de oceane

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'oceane': { 
+        'ingredients': {'saumon': 2, 'citron': 4, 'onion': 3},
+        'price': 12,
     }
 }
